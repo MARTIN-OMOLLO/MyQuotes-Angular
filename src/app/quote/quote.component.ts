1b337918b11688972685682af1431b819253163b
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-02-08 21:18:39 +0300

    add authors

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
   quotes:Quotes[] = [
     new Quotes (1, 'Martin', 'Love', 'I love you for all that you are, all that you have been and all that you will be', 'Martin Omollo', new Date(2021,12,3), 0, 0),
     new Quotes (2, 'David', 'Technology', 'Technology is the best when it brings people together','David Odawa', new Date(2022,1,1), 0, 0),
-    new Quotes (3, 'Lawrence', 'Motivational', 'The greatest glory in living lies not in never falling but in rising every time we fail','Maya Angelou',new Date(2021,3,1), 0, 0)
+    new Quotes (3, 'Lawr', 'Motivational', 'The greatest glory in living lies not in never falling but in rising every time we fail','Maya Angelou',new Date(2021,3,1), 0, 0)
 
   ]
   toggleDetails(index:any){this.quotes[index].showDescription=!this.quotes[index].showDescription}
